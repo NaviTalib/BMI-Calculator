8a31e553f82da167eb64b37f0699b5ef79042955
Navi Talib <talibali786.ta@gmail.com>
2023-03-16 19:36:34 +0530

    bmicalculator.js is update

--- bmicalculator.js
+++ bmicalculator.js
@@ -1,8 +1,19 @@
-function BMI(weight,height){
+function bmiCal(weight,height){
 
     var bmi = weight/Math.pow(height,2);
-    return Math.round(bmi);
+        // return Math.round(bmi);
+
+    if(bmi<=18.5){
+        return "so you are underweight."
+               }
+     else if(bmi>18.5 && bmi<=24.9){
+        return "so you have a normal weight."
+     }
+     else if(bmi>24.9){
+        return "so you are overweight"
+     }
+
 
 };
 
-console.log(BMI(65,1.8));
+console.log(bmiCal(65,1.8))
